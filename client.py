e79409479392507a5c470face3f0c78caafbb270
sifti-grover <groversifti@gmail.com>
2024-11-15 23:09:06 +0530

    minor changes

--- client.py
+++ client.py
@@ -1,9 +1,8 @@
 import socket
 import threading
 
-HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT =  5000       # Port to listen on (non-privileged ports are > 1023)
-
+HOST = '127.0.0.1'  
+PORT =  5000       
 def send_messages(client_socket):
     while True:
         try:
@@ -34,20 +33,7 @@ def receive_messages(client_socket):
             client_socket.close()
             break
 
-# def start_client():
-#     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
-#         try:
-#             client_socket.connect((HOST, PORT))
-#             print("Connected to the server!")
 
-#             # Start send and receive threads
-#             threading.Thread(target=send_messages, args=(client_socket,)).start()
-#             threading.Thread(target=receive_messages, args=(client_socket,)).start()
-
-#         except ConnectionRefusedError:
-#             print("Connection refused. Is the server running?")
-#         except Exception as e:
-#             print("An error occurred:", e)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
     try:
@@ -67,17 +53,3 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
     finally:
         client_socket.close()
 
-# if __name__ == "__main__":
-#     start_client()
-#     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#     client_socket.connect((HOST, PORT))
-#     print("Connected to the server!")
-
-#     # Start send and receive threads
-#     threading.Thread(target=send_messages, args=(client_socket,)).start()
-#     threading.Thread(target=receive_messages, args=(client_socket,)).start()
-
-# except ConnectionRefusedError:
-#     print("Connection refused. Is the server running?")
-# except Exception as e:
-#     print("An error occurred:", e)
